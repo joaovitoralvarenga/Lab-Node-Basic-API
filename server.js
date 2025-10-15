0b1823abf26632f95cf43cf6b8c10b03083c9da1
joaovitoralvarenga <ggquennbr@gmail.com>
2025-10-15 08:16:56 -0300

    alt:atualizção

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/app', express.static(path.join(__dirname, '/public')))
 
 let port = process.env.PORT || 3000
-app.listen(port)
+app.listen(PORT, () => { console.log(`Server running on port ${port}`) })
 
 const apiRouter = require('./api/routes/apiRouter.js')
 
